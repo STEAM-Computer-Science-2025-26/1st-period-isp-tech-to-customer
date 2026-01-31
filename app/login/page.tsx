9e8dd77eae94100fab4896c6cac7bd2ec11a0f54
lunar-arch <barcroft.j.nathan@gmail.com>
2026-01-31 11:15:14 -0600

    feat: add padding and overflow handling to login page layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/layout/LoginForm";
 
 export default function LoginPage() {
 	return (
-		<div className="w-full h-screen flex items-center justify-center">
+		<div className="w-full h-screen flex items-center justify-center px-10 overflow-hidden">
 			<LoginForm registering={false} />
 		</div>
 	);
